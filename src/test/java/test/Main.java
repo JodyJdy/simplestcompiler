25a2233d569bf46d0755e24007590fcc05cf74da
jdy <1154206436@qq.com>
2025-05-08 16:35:16 +0800

    支持交互式运行

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -1,18 +1,8 @@
 package test;
 
 import com.example.compiler.Starter;
-import com.example.compiler.grammer.Env;
-import com.example.compiler.grammer.Executor;
-import com.example.compiler.grammer.Parser;
-import com.example.compiler.grammer.Statement;
-import com.example.compiler.lexer.Token;
-import com.example.compiler.lexer.TokenGenerator;
 
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
